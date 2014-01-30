b83cd8ef3469960b851ca1c74ee2ffdeb4874356
polsVoice <brent.bartlett1@gmail.com>
2014-01-30 11:02:44 -0800

    update game with CSS

--- assignment_4/game.js
+++ assignment_4/game.js
@@ -3,9 +3,19 @@
 	var map = [];
 
 	map[ 0 ] = "A well-lit room.";
-	map[ 1 ] = "A sparse kitchen.";
-
+	map[ 1 ] = "A sparse kitchen.<br /> There is a toaster here.";
+	
+	actions = [ "north", "south", "east", "west" ];
+	
+	var theInput = "";
+	var theAction = "";
+	
+	var items = [ "flat pastry" ];
+	var itemLocations = [ 0 ];
+	
 	var input = document.querySelector( "#input" );
+	input.focus();
+	
 	var output = document.querySelector( "#output" );
 	var button = document.querySelector( "button" );
 	button.addEventListener( "click", clickHandler, false );
@@ -16,10 +26,21 @@
 
 	function clickHandler()
 	{
-		var action = input.value;
+		theInput = input.value.toLowerCase();
 		
+		// detect if there is a game action in the user's input
+		for( var i = 0, ii = actions.length; i < ii; i++ )
+		{
+			if( theInput.indexOf( actions[ i ] ) !== -1 )
+			{
+				theAction = actions[ i ];
+				break;
+			}
+		}
+		
+		// player movement
 		message = "";
-		switch( action )
+		switch( theAction )
 		{
 			case "north":
 				if( mapLocation < 1 )
@@ -28,12 +49,12 @@
 				}
 				else
 				{
-					message = "You can't go that way!";
+					message = "<br />You can't go that way!";
 				}
 				break;
 			
 			case "east":
-				message = "You can't go that way!";
+				message = "<br />You can't go that way!";
 				break;
 				
 			case "south":
@@ -43,23 +64,46 @@
 				}
 				else
 				{
-					message = "You can't go that way!";
+					message = "<br />You can't go that way!";
 				}
 				break;
 			
 			case "west":
-				message = "You can't go that way!";
+				message = "<br />You can't go that way!";
 				break;
 				
+			/*case "take":
+				take();
+				break;
+				
+			case "use":
+				use();
+				break;
+				*/
 			default:
 				message = "I don't understand you.";
 		}
 		narrate();
+		// erase text field and return focus to it
+		input.value = "";
+		input.focus();
+	}
+	
+	function take()
+	{
+		
 	}
 
 	function narrate()
 	{
+		// output description of location
 		output.innerHTML = map[ mapLocation ];
+		//description of item at current location (if applicable)
+		for( var i = 0, ii = items.length; i < ii; i++ )
+		{
+			if( mapLocation === itemLocations[ i ] )
+				output.innerHTML += "<br />You see a " + items[ i ] + ".";
+		}
 		output.innerHTML += "<br />" + message;
 	}
 }() );
